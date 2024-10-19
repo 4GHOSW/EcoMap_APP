f120bd1ae9fd38e80700b5e6aa75f7fe1ad76c3f
Taewoong-H <taeung2008@gmail.com>
2024-10-19 21:26:23 +0900

    feat: 첫 RN 개발환경

--- App.tsx
+++ App.tsx
@@ -1,118 +1,38 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+import {View, Text, TouchableOpacity, SafeAreaView} from 'react-native';
+// import {NaverMapView} from '@mj-studio/react-native-naver-map';
 
 function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+    <SafeAreaView className="flex-1">
+      {/* <NaverMapView
+        onInitialized={() => {
+          return console.log('initialized!');
+        }}
+      /> */}
+      <View className="absolute top-5 left-5 right-5 flex-row bg-white rounded-full shadow-md">
+        <TouchableOpacity className="flex-1 py-3 px-5">
+          <Text className="text-center text-gray-700">출발입니다</Text>
+        </TouchableOpacity>
+        <TouchableOpacity className="flex-1 py-3 px-5">
+          <Text className="text-center text-gray-700">도착입니다</Text>
+        </TouchableOpacity>
+      </View>
+      <View className="absolute bottom-0 left-0 right-0 bg-white p-5">
+        <Text className="text-center mb-4 text-gray-700">
+          로그인 시 절약한 탄소만큼 받을 수 있는 혜택이 있어요!
+        </Text>
+        <View className="flex-row justify-between">
+          <TouchableOpacity className="flex-1 py-3 bg-gray-200 rounded-md mr-2">
+            <Text className="text-center text-black">회원가입</Text>
+          </TouchableOpacity>
+          <TouchableOpacity className="flex-1 py-3 bg-blue-500 rounded-md ml-2">
+            <Text className="text-center text-white">로그인</Text>
+          </TouchableOpacity>
         </View>
-      </ScrollView>
+      </View>
     </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
